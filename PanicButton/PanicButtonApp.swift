32855e0b9031feae09fbe43a1c4a13a84d6efc21
Lucas Barker <camerastooge@gmail.com>
2025-11-09 10:19:00 -0600

    Initial Commit

---
+++ PanicButton/PanicButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanicButtonApp.swift
+//  PanicButton
+//
+//  Created by Lucas Barker on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PanicButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
